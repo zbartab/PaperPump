01df9f861d8c7d5e8d18b28ad30ad9ec77840f9c
Zoltan Barta <barta.zoltan@science.unideb.hu>
2019-11-13 18:30:06 +0100

    R script files added
    These are used to produce the plots in the MS.

---
+++ groups.R
@@ -0,0 +1,48 @@
+
+# produce a figure for the author groups
+
+library(igraph)
+
+create.network <- function(gr.name, gr.size, n.coauthors, gr.connected=FALSE) {
+	group <- paste(gr.name, LETTERS[1:gr.size], sep=".")
+	coauthors <- sapply(group,
+											function(a) paste(a, letters[1:n.coauthors], sep="."))
+	coauthors <- as.vector(coauthors)
+	edges <- data.frame(authors=rep(group, rep(n.coauthors, gr.size)),
+											coauthors=coauthors)
+	edges <- as.matrix(edges)
+	if(gr.connected) {
+		i <- length(group)
+		p <- (i*(i-1))/2
+		ee <- matrix("", ncol=2, nrow=p)
+		k <- 1
+		for(i in 1:(gr.size-1)) {
+			for(j in (i+1):gr.size) {
+				ee[k, 1] <- group[i]
+				ee[k, 2] <- group[j]
+				k <- k+1
+			}
+		}
+		edges <- rbind(edges, ee)
+	}
+	g <- graph_from_edgelist(edges, directed=FALSE)
+	V(g)$size <- 30
+	V(g)$color[grepl("[a-c]$", names(V(g)))] <- "salmon"
+	V(g)$color[grepl("[^a-c]$", names(V(g)))] <- "lightgreen"
+	E(g)$color <- "black"
+	E(g)$width <- 4
+	g
+}
+G.A <- G.B <- 4 # group size
+c.A <- c.B <- 3 # number of coauthors from outside
+network.A <- create.network("A", G.A, c.A)
+network.B <- create.network("B", G.B, c.B, TRUE)
+coords <- layout_(network.B, nicely())
+png(file="groups.png", width=700, height=400)
+op <- par(mfcol=c(1,2))
+plot(network.A, layout=coords, main="group A",
+		 mark.groups=list(V(network.A)[nchar(names(V(network.A))) == 3]))
+plot(network.B, layout=coords, main="group B",
+		 mark.groups=list(V(network.B)[nchar(names(V(network.B))) == 3]))
+par(op)
+dev.off()
---
+++ weighted_production.R
@@ -0,0 +1,51 @@
+
+# figure to illustrate the weighted publication production
+
+## functions
+weighted.pubs.A <- function(c.co, a=3, G=4) {
+	a/(1+c.co)
+}
+weighted.pubs.B <- function(c.co, a=3, G=4) {
+	(G*a)/(G+c.co)
+}
+w.p.A <- function(co, a=3, G=4, b=0.2) {
+	a*b + (1-b)*(a/(1+co))
+}
+w.p.B <- function(co, a=3, G=4, b=0.2) {
+	a*b + (1-b)*((G*a)/(G+co))
+}
+
+## plots
+
+png(file="weighted_production.png", width=700, height=600)
+op <- par(mfcol=c(1, 2))
+cc <- seq(0,10,0.01)
+plot(cc, weighted.pubs.A(cc), type="l", bty="l",
+		 xlab=expression(paste("number of collaborators, ", italic(c))),
+		 ylab=expression(paste("weighted publication performance, ", italic(w)[i])))
+lines(cc, weighted.pubs.B(cc), col=2)
+lines(cc, w.p.A(cc), lty=2)
+lines(cc, w.p.B(cc), lty=2, col=2)
+legend("topright", legend=c("author A", "author A with bonus",
+														"author B", "author B with bonus"), lty=c(1,2,1,2),
+														col=c(1,1,2,2), bty="n")
+mtext("a", side=3, adj=0.1)
+
+ww0 <- weighted.pubs.B(cc)/weighted.pubs.A(cc)
+ww02 <- w.p.B(cc)/w.p.A(cc)
+ww05 <- w.p.B(cc, b=0.5)/w.p.A(cc, b=0.5)
+plot(cc, ww0, type="l", bty="l",
+		 xlab=expression(paste("number of collaborators, ", italic(c))),
+		 ylab=expression(paste("relative weighted performance, ",
+													 italic(w)[B]/italic(w)[A])))
+lines(cc, ww02, lty=2)
+lines(cc, ww05, lty=2)
+legend("topleft", legend=c("no bonus", "first authorship bonus"),
+			 lty=c(1,2), bty="n")
+mtext(expression(italic(b) == "0.0"), side=4, at=ww0[length(ww0)])
+mtext(expression(italic(b) == "0.2"), side=4, at=ww02[length(ww02)])
+mtext(expression(italic(b) == "0.5"), side=4, at=ww05[length(ww05)])
+mtext("b", side=3, adj=0.1)
+par(op)
+dev.off()
+
