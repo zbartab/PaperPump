60d006d0bb6283bf86fb45cc62ff7e33d4fef8be
Zoltan Barta <barta.zoltan@science.unideb.hu>
2019-12-19 20:07:45 +0100

    Scripts modified to process dblp publication data
    - Makefile now records the source of the publication data
    - the perl script generates a simple txt representation of the dblp
    publication data
    - the R script processes this representation to make a list of the data

--- analyse-publications/dblp-data/parse-dblp_xml.R
+++ analyse-publications/dblp-data/parse-dblp_xml.R
@@ -1,10 +1,21 @@
 
-library(XML)
 
 
-parsed.object <- "parsed-dblp.Rdata"
 parsed.list <- "parsed-dblp-list.Rdata"
-d.parsed <- xmlParse("dblp-articles.xml")
-save(d.parsed, file=parsed.object)
-dblp.ls <- xmlToList(d.parsed)
+
+dblp.file <- scan(file="dblp-articles.txt", what=character(), sep="\n")
+n.records <- length(dblp.file)/3
+#n.records <- 10
+dblp.ls <- vector(mode = "list", length = n.records)
+i <- 1:n.records
+i <- (i*3) - 2
+n <- sub("^doi\\.org/", "", dblp.file[i])
+names(dblp.ls) <- n
+for(i in 1:n.records) {
+	j <- i*3
+	dblp.ls[[i]] <- list(authors=unlist(strsplit(dblp.file[j-1], "; ")),
+											 year=as.numeric(dblp.file[j]))
+	#if(i %% 10000 == 0) {print(i)}
+}
+
 save(dblp.ls, file=parsed.list)
