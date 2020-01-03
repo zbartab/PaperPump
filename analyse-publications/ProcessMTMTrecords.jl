3390766ff8c8eed3554756e9d8ca08efdc84aea6
Zoltan Barta <barta.zoltan@science.unideb.hu>
2020-01-03 22:33:37 +0100

    Processing MTMT records implemented
    - function to read and process MTMT records added

--- analyse-publications/ProcessMTMTrecords.jl
+++ analyse-publications/ProcessMTMTrecords.jl
@@ -16,3 +16,36 @@ function read_MTMT(file)
 	end
 	return r["content"]
 end
+
+"""
+    recordauthors(filelist)
+
+Record the papers written by authors stored in MTMT json files in
+`filelist`.
+"""
+function recordauthors(filelist::Array{String,1})
+	authorrecs = Dict{UInt64, Array{UInt64,1}}()
+	authorIDs = Dict{UInt64, String}()
+	for f in filelist
+		(recs = read_MTMT(f)) == nothing && continue
+		idstr = replace(f, r".*/(.*)_.*$" => s"\1")
+		id = hash(idstr)
+		authorIDs[id] = idstr
+		!haskey(authorrecs, id) && (authorrecs[id] = Array{UInt64}[])
+		for r in recs
+			hasranking(r) && push!(authorrecs[id], hash(r["mtid"]))
+		end
+		length(authorrecs[id]) == 0 && delete!(authorrecs, id)
+	end
+	return authorrecs, authorIDs
+end
+
+"""
+    hasranking(cikk)
+
+Returns `true` if MTMT paper record `cikk` has journal ranking field.
+"""
+function hasranking(cikk)
+	return haskey(cikk, "ratings") && haskey(cikk["ratings"][1], "ranking")
+end
+
