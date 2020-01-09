64345d705890563c814f8ccaaabcb33a2f0d29fa
Zoltan Barta <barta.zoltan@science.unideb.hu>
2020-01-09 22:49:25 +0100

    Code refactory
    - the code processing the dblp data is refactored to conform to the
    PubMat type

--- analyse-publications/dblp/ProcessDBLPrecords.jl
+++ analyse-publications/dblp/ProcessDBLPrecords.jl
@@ -28,13 +28,14 @@ Record the authors who write the papers in the dblp database, passed as
 function recordpapers(lines::Array{String,1}, startyear::Int,
 											stopyear::Int)
 	n_records = div(length(lines), 3)
-	dblp_dict = Dict{UInt64, Dict{String, Union{Int, Array{UInt64,1}}}}()
+	dblp_dict = Dict{String, Dict{String, Union{Int, Array{String,1}}}}()
 	for i in 1:n_records
 		j = i * 3
 		year = parse(Int, lines[j])
 		startyear <= year <= stopyear || continue
-		doi = hash(lines[j-2])
-		dblp_dict[doi] = Dict("authors" => hash.(split(lines[j-1], "; ")),
+		#doi = hash(lines[j-2])
+		doi = replace(lines[j-2], r"^doi\.org/" => "")
+		dblp_dict[doi] = Dict("authors" => String.(split(lines[j-1], "; ")),
 													"year" => year)
 	end
 	return dblp_dict
@@ -45,10 +46,10 @@ end
 
 Return a dict of unique paper IDs.
 """
-function collectauthors(aurecs::Dict{UInt64,
+function collectauthors(aurecs::Dict{String,
 																		Dict{String,Union{Int64,
-																										 Array{UInt64,1}}}})
-	authors = Dict{UInt64,Int}()
+																										 Array{String,1}}}})
+	authors = Dict{String,Int}()
 	for ps in values(aurecs)
 		for p in ps["authors"]
 			authors[p] = 1
@@ -67,10 +68,10 @@ end
 
 Return a dict of unique papers IDs.
 """
-function collectpapers(aurecs::Dict{UInt64,
+function collectpapers(aurecs::Dict{String,
 																		 Dict{String,Union{Int64,
-																											 Array{UInt64,1}}}})
-	papers = Dict{UInt64,Int}()
+																											 Array{String,1}}}})
+	papers = Dict{String,Int}()
 	i = 1
 	for k in keys(aurecs)
 		papers[k] = i
@@ -87,17 +88,22 @@ the result of `recordpapers`). It returns (as a tuple) the publication
 matrix and a hash table to allow the identification of authors in the
 publication matrix.
 """
-function recs2pubmat(aurecs::Dict{UInt64,
+function recs2pubmat(aurecs::Dict{String,
 																	Dict{String,Union{Int64,
-																										Array{UInt64,1}}}})
+																										Array{String,1}}}})
 	ps = collectpapers(aurecs)
 	as = collectauthors(aurecs)
 	pubmat = spzeros(length(ps), length(as))
+	paperIDs = Array{String,1}(undef, length(ps))
+	authorIDs = Array{String,1}(undef, length(as))
 	for k in keys(aurecs)
 		p_i = ps[k]
+		paperIDs[p_i] = k
 		for a in aurecs[k]["authors"]
-			pubmat[p_i, as[a]] = 1
+			ai = as[a]
+			pubmat[p_i, ai] = 1
+			authorIDs[ai] = a
 		end
 	end
-	return pubmat, as
+	return PubMat(pubmat, authorIDs, paperIDs)
 end
