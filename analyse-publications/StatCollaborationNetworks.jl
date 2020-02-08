457a47971f60216eafa6864f5d9b7b14b43dbaf7
Zoltan Barta <barta.zoltan@science.unideb.hu>
2020-02-08 18:20:53 +0100

    StatCollaborationNetworks.jl modified
    - new, probably the correct version of ECCDF added

--- analyse-publications/StatCollaborationNetworks.jl
+++ analyse-publications/StatCollaborationNetworks.jl
@@ -153,6 +153,12 @@ function eCCDF(x::Array{Int, 1})
 	y = vcat(1, y[1:(end-1)])
 	return xs, y
 end
+function eCCDF2(x::Array{Number, 1})
+	xs = sort(x)
+	y = collect(length(xs):-1:1) ./ length(xs)
+	#y = vcat(1, y[1:(end-1)])
+	return xs, y
+end
 
 """
     generate_rndnet(k, p, cartel, pc)
