ad80496814d95ea163f99bcc3567f6dcdc0a8233
Zoltan Barta <barta.zoltan@science.unideb.hu>
2020-02-20 20:19:51 +0100

    StatCollaborationNetworks.jl modified
    - a new version of empirical complementary cummulative distribution
    function added

--- analyse-publications/StatCollaborationNetworks.jl
+++ analyse-publications/StatCollaborationNetworks.jl
@@ -153,6 +153,12 @@ function eCCDF(x::Array{Int, 1})
 	y = vcat(1, y[1:(end-1)])
 	return xs, y
 end
+function eCCDF2(x::Array{Int, 1})
+	xs = sort(x)
+	y = collect(length(xs):-1:1) ./ length(xs)
+	#y = vcat(1, y[1:(end-1)])
+	return xs, y
+end
 function eCCDF2(x::Array{Float64, 1})
 	xs = sort(x)
 	y = collect(length(xs):-1:1) ./ length(xs)
