cf2e49bafb35406e966f5b4a83966148e7ce0a14
Zoltan Barta <barta.zoltan@science.unideb.hu>
2020-03-03 15:35:10 +0100

    New umbrella file created to facilitate loading julai code

---
+++ analyse-publications/PaperPump.jl
@@ -0,0 +1,9 @@
+
+#module PaperPump
+
+include("CollaborationNetworks.jl")
+include("StatCollaborationNetworks.jl")
+include("RandomPublicationNetworks.jl")
+include("PlotCollaborationNetworks.jl")
+
+#end
